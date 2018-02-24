e792da1bb730fad32987f27f63bba44b25f493cb
kellybarber <kelly.james.barber@gmail.com>
2018-02-24 16:00:40 -0500

    stripped down app to boilerplate

--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ import IndecisionApp from './components/IndecisionApp'
 import './styles/styles.scss'
 import 'normalize.css/normalize.css'
 
-ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
--- src/playground/es6-let-const.js
+++
@@ -1,6 +0,0 @@
-var nameVar = 'Andrew'
-var nameVar = 'Mike'
-console.log('nameVar', nameVar)
-
-let nameLet = 'Jen'
-console.log('Jen', Jen)
