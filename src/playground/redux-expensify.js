377b392e99dd173a378c3e9e99df3c35e8ad7c7e
kellybarber <kelly.james.barber@gmail.com>
2018-03-04 20:43:08 -0500

    added sort functions for amount and date

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -40,7 +40,15 @@ const setTextFilter = (text = '') => ({
 })
 
 // SORT_BY_DATE
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE',
+})
+
 // SORT_BY_AMOUNT
+const sortByAmount = () => ({
+  type: 'SORT_BY_AMOUNT',
+})
+
 // SET_START_DATE
 // SET_END_DATE
 
@@ -85,6 +93,16 @@ const filterReducer = (state = filterReducerDefaultState, action) => {
         ...state,
         text: action.text
       }
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date'
+      }
+    case 'SORT_BY_AMOUNT':
+      return {
+        ...state,
+        sortBy: 'amount'
+      }
     default: 
       return state
   }
@@ -110,8 +128,8 @@ store.dispatch(editExpense( expenseTwo.expense.id, { amount: 500 } ))
 store.dispatch(setTextFilter('rent'))
 store.dispatch(setTextFilter())
 
-
-
+store.dispatch(sortByAmount())
+store.dispatch(sortByDate())
 
 
 // const demoState = {
