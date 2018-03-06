fc92c05e5fa426f6a3e445c919a4507b4c910fa3
kellybarber <kelly.james.barber@gmail.com>
2018-03-06 12:35:05 -0500

    imported and tested functions in main app file

--- src/actions/filters.js
+++ src/actions/filters.js
@@ -1,27 +1,27 @@
 // SET_TEXT_FILTER
-const setTextFilter = (text = '') => ({
+export const setTextFilter = (text = '') => ({
   type: 'SET_TEXT_FILTER',
   text
 })
 
 // SORT_BY_DATE
-const sortByDate = () => ({
+export const sortByDate = () => ({
   type: 'SORT_BY_DATE',
 })
 
 // SORT_BY_AMOUNT
-const sortByAmount = () => ({
+export const sortByAmount = () => ({
   type: 'SORT_BY_AMOUNT',
 })
 
 // SET_START_DATE
-const setStartDate = (startDate = undefined) => ({
+export const setStartDate = (startDate = undefined) => ({
   type: 'SET_START_DATE',
   startDate
 })
 
 // SET_END_DATE
-const setEndDate = (endDate = undefined) => ({
+export const setEndDate = (endDate = undefined) => ({
   type: 'SET_END_DATE',
   endDate
 })
--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
-import React     from 'react'
-import ReactDOM  from 'react-dom'
-import AppRouter from './routers/AppRouter'
+import React              from 'react'
+import ReactDOM           from 'react-dom'
+import AppRouter          from './routers/AppRouter'
+import configureStore     from './store/configureStore'
+import { addExpense }     from './actions/expenses'
+import { setTextFilter }  from './actions/filters'
+import expensesReducer    from './reducers/expenses'
+import filtersReducer     from './reducers/filters'
+import getVisibleExpenses from './selectors/expenses'
 import './styles/styles.scss'
 import 'normalize.css/normalize.css'
+
+const store = configureStore()
+
+store.subscribe(() => {
+  const state = store.getState()
+  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+  console.log(visibleExpenses)
+})
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 3000, createdAt: 21000 }))
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 5000, createdAt: 20000 }))
+
+store.dispatch(setTextFilter( 'bill' ))
+
 
 ReactDOM.render(<AppRouter/>, document.getElementById('app'));
