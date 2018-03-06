630f67373e664cd63c1f9baee9184c2720a3a245
kellybarber <kelly.james.barber@gmail.com>
2018-03-06 11:37:04 -0500

    added sort function to return filtered values sorted by either date or amount

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -66,9 +66,15 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
   return expenses.filter((expense) => {
     const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate
     const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate
-    const textMatch = expense.description.toLowerCase().includes(text)
+    const textMatch = expense.description.toLowerCase().includes(text.toLowerCase())
 
     return startDateMatch && endDateMatch && textMatch
+  }).sort((a, b) => {
+    if (sortBy === 'date') {
+      return a.createdAt < b.createdAt ? 1 : -1
+    } else if (sortBy === 'amount') {
+      return a.amount < b.amount ? 1 : -1
+    }
   })
 }
 
@@ -151,16 +157,16 @@ store.subscribe(() => {
   console.log(visibleExpenses)
 })
 
-const expenseOne = store.dispatch(addExpense({ description: 'rent', amount: 50000, createdAt: 1000 }))
+const expenseOne = store.dispatch(addExpense({ description: 'rent', amount: 50000, createdAt: -21000 }))
 const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000 }))
 
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }))
 // store.dispatch(editExpense( expenseTwo.expense.id, { amount: 500 } ))
 
-store.dispatch(setTextFilter('co'))
+// store.dispatch(setTextFilter('co'))
 // store.dispatch(setTextFilter())
 
-// store.dispatch(sortByAmount())
+store.dispatch(sortByAmount())
 // store.dispatch(sortByDate())
 
 // store.dispatch(setStartDate(-500))
