f0f7b2b06a7b493265804b17571a54cd23559e87
kellybarber <kelly.james.barber@gmail.com>
2018-03-15 11:43:48 -0400

    set up expense form to work with edit expense and connected it to the store also moved remove expense button to edit expense page

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,20 +1,13 @@
 import React from 'react'
-import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { removeExpense } from '../actions/expenses'
 
-const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt }) => (
   <div>
     <Link to={`/edit/${id}`}>
       <h3>{description}</h3>
     </Link>
     <p>Amount: {amount}</p>
     <p>Created At: {createdAt}</p>
-    <button onClick={() => {
-      dispatch(removeExpense({ id }))
-    }}>
-      Remove
-    </button>
   </div>
 )
-export default connect()(ExpenseListItem)
+export default ExpenseListItem
