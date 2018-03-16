c7866eeb1dfde430429881e8cc39f01ff78cc7ea
kellybarber <kelly.james.barber@gmail.com>
2018-03-16 14:04:51 -0400

    changed build to put css in its own file and used options with loader to get better stack trace

--- src/app.js
+++ src/app.js
@@ -8,18 +8,19 @@ import { setTextFilter }  from './actions/filters'
 import getVisibleExpenses from './selectors/expenses'
 import './styles/styles.scss'
 import 'normalize.css/normalize.css'
+import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore()
 
-store.subscribe(() => {
-  const state = store.getState()
-  const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
-  console.log(visibleExpenses)
-})
+// store.subscribe(() => {
+//   const state = store.getState()
+//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+//   console.log(visibleExpenses)
+// })
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 5000, createdAt: 1000 }))
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 3000, createdAt: 33450000 }))
-store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 103300 }))
+// store.dispatch(addExpense({ description: 'Water Bill', amount: 5000, createdAt: 1000 }))
+// store.dispatch(addExpense({ description: 'Gas Bill', amount: 3000, createdAt: 33450000 }))
+// store.dispatch(addExpense({ description: 'Rent', amount: 109500, createdAt: 103300 }))
 
 const jsx = (
   <Provider store={store}>
